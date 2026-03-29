cb191a13600b9d1aa94bbaf325dca4e094d407fd
Sumudu Hettiarachchi <shettiarachchi@adsl-dynamic-ipv6.slt.lk>
2026-03-29 11:26:18 +0530

    Feat improve authentication security
    - Implemented password hashing using Werkzeug
    - Updated login logic to use check_password_hash
    - Recreated users with hashed passwords
    - Enhanced overall authentication security"

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -2,6 +2,8 @@ from flask import Blueprint, render_template, request, redirect, url_for
 from app.models import User
 from app.extensions import db
 from flask_login import login_user, logout_user
+from werkzeug.security import check_password_hash
+
 
 auth_bp = Blueprint('auth', __name__)
 
@@ -10,7 +12,7 @@ def login():
     if request.method == "POST":
         user = User.query.filter_by(username=request.form["username"]).first()
 
-        if user and user.password == request.form["password"]:
+        if user and check_password_hash(user.password, request.form["password"]):
             login_user(user)
             return redirect(url_for("courses.list_courses"))
 
